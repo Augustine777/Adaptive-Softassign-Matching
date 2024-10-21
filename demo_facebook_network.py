217642465d78b05b4cc5896da7edae74853a64d9
Binrui Shen <40590957+BinruiShen@users.noreply.github.com>
2024-10-21 11:45:34 +0800

    Update demo_facebook_network.py

--- demo_facebook_network.py
+++ demo_facebook_network.py
@@ -1,17 +1,12 @@
 import networkx
 import numpy as np
-import ASM as *
+From ASM import *
 
 
 G_face = networkx.read_gpickle("networks/facebook/G_face.gpickle")
 G_face_noise_5 = networkx.read_gpickle("networks/facebook/G_face_noise_5.gpickle")
 G_face_noise_15 = networkx.read_gpickle("networks/facebook/G_face_noise_15.gpickle")
 G_face_noise_25 = networkx.read_gpickle("networks/facebook/G_face_noise_25.gpickle")
-
-adj = networkx.to_numpy_matrix(G_face)
-adj_5 = networkx.to_numpy_matrix(G_face_noise_5)
-adj_15 = networkx.to_numpy_matrix(G_face_noise_15)
-adj_25 = networkx.to_numpy_matrix(G_face_noise_25)
 
 adj = networkx.to_numpy_matrix(G_face)
 adj_5 = networkx.to_numpy_matrix(G_face_noise_5)
